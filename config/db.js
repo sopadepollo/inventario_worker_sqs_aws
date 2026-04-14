2801376aa866a4f6466927793c9eb1364275b80d
aa5974076@gmail.com <aa5974076@gmail.com>
2026-04-13 18:14:12 -0600

    fix: anadicion del archivo config faltante

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+module.exports = {pool}
