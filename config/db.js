9ae40602e36b89b92603edbb3b77cc2a73077ee8
aa5974076@gmail.com <aa5974076@gmail.com>
2026-04-22 11:44:38 -0600

    fix:anadicion del pool correcto

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const {Pool} = require('pg');
 
-const pool = new Pool({
+const writePool = new Pool({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
-	port: process.env.DB_PORT,
+	port: process.env.DB_PORT || 5432,
 	ssl: {
 		rejectUnauthorized: false
 	}
 });
 
-module.exports = {pool}
+module.exports = {writePool}
